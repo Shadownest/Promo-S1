03f990d2e6f27d117547575c8fcc653b98a223df
Bastien <bastien-bardou@sfr.fr>
2014-11-27 11:57:15 +0100

    Ajout usermanager

--- app/register.php
+++ app/register.php
@@ -1,1 +1,17 @@
-<?php require("views/register.phtml"); ?>
+<?php
+
+if(isset($_POST["login"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["password2"]))
+{
+	$manager=new UserManager($db);
+	$user=$manager->register($_POST["login"],$_POST["email"],$_POST["password"],$_POST["password2"]);
+	if (gettype($user) != "string"){
+		header('Location: index.php?page=login');
+		exit();
+	}
+	else{
+		$error = $user;
+	}
+}
+
+require("views/register.phtml"); 
+?>
