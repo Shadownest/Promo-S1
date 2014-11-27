a038cdd652107a1d6328a3aef7233bc7c5b0bc21
Bastien <bastien-bardou@sfr.fr>
2014-11-27 11:47:24 +0100

    MAJ config.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@ define("MYSQL_USER", 'forum');
 define("MYSQL_PASSWORD", 'forum');
 define("MYSQL_HOST", '10.32.195.171');
 define("MYSQL_DATABASE", 'forum');
+
+$error="";
 ?>
---
+++ models/UserManager.class.php
@@ -0,0 +1,70 @@
+<?php 
+class UserManager{
+	private $db;
+
+	public function __construct($db){
+		$this->db=$db;
+	}
+
+	public function register($name,$email,$password,$password2){
+		if($password==$password2){
+			if (strlen($name) < 4){
+				return 'Login trop court';
+			}
+			else if (strlen($password) < 4){
+				return 'Mot de passe trop court';
+			}
+			else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				return 'email invalide';
+			}
+			else{
+				$name = mysqli_real_escape_string($this->db, $name);
+				$password = password_hash($password, PASSWORD_BCRYPT, ['cost'=>14]);
+
+				$verif=mysqli_query($this->db, "SELECT name, password, email FROM user WHERE name='".$name."'");
+				$result=mysqli_num_rows($verif);
+				if($result==0){
+					$request=mysqli_query($this->db,"INSERT INTO user (name, password, email) VALUES ('".$name."', '".$password."', '".$email."')");
+					$res=mysqli_query($this->db, "SELECT name, password, email FROM user WHERE name='".$name."'AND password='".$password."' AND email='".$email."'");
+
+					if($res){
+						$user=mysqli_fetch_object($res, "User");
+						if($user){
+							return $user;
+						}
+					}
+					exit;
+				}
+				else{
+					return "Le login est déjà pris";
+				}
+			}
+		}
+		else{
+			return "Les mots de passe ne correspondent pas";
+		}
+	}
+
+	public function login($name,$password){
+		$name= mysqli_real_escape_string($this->db, $name);
+
+		$res=mysqli_query($this->db, "SELECT id, name, password, level, avatar, description FROM user WHERE name='".$name."'");
+
+		if($res){
+			$user=mysqli_fetch_object($res,"User");
+			if($user){
+				if($user->verifPassword($password)){
+					return $user;
+				}
+				else{
+					return "Mot de passe incorrect";
+				}
+			}
+			else return "Votre identifiant ne correspond pas";
+		}
+		else{
+			return "Erreur interne";
+		}
+	}
+}
+?>
