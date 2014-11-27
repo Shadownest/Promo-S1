aaea1a11bd6e3217ccddf078329579ec2f1a6652
micado <michaelcorbineau@gmail.com>
2014-11-27 16:38:08 +0100

    maj catego

--- app/ucp_admin.php
+++ app/ucp_admin.php
@@ -1,8 +1,45 @@
 <?php
 
+if (isset($_SESSION['admin']) && $_SESSION['admin'] == TRUE){
+
+	if (isset($_POST['categories_admin'])){
+
+		$manager = new CategoryManager($db);
+		$category = $manager->categoriesAdmin($_POST['categories_admin']);
+
+		header("Location: index.php?page=ucp_admin");
+		exit();
+	}
+
+	if(isset($_POST['delete_category'])){
+
+		$manager = new CategoryManager($db);
+		$category = $manager->deleteCategory($_POST['delete_category']);
+
+		header("Location: index.php?page=ucp_admin");
+		exit();
+
+	}
+
+	if(isset($_POST['create_category'])){
+
+		$manager = new CategoryManager($db);
+		$category = $manager->createCategory($_POST['create_category']);
+
+		header("Location: index.php?page=ucp_admin");
+		exit();
+	}
 
 
 
-require("views/ucp_admin.phtml");
+
+	else{
+		require("views/ucp_admin.phtml");
+	}
+ }
+else{
+	header("Location: index.php?page=home");
+} 
+
 
 ?>
--- models/CategoryManager.class.php
+++ models/CategoryManager.class.php
@@ -9,6 +9,29 @@ class CategoryManager{
 		$this->db = $db;
 
 	}
+
+	public function categoriesAdmin(){
+
+	$res=mysqli_query($this->db, "SELECT title from category");
+	if($res){
+	 	$category = mysqli_fetch_object($res, "Category");
+	 	return $category; 
+	 	}
+		return null;
+	}
+
+	public function deleteCategory(){
+
+	mysqli_query($this->db, "DELETE FROM category WHERE ");
+		
+	}
+
+	public function createCategory(){
+
+	//mysqli_query($this->db, "INSERT INTO `forum` ");
+		
+	}
+
 }
 
 ?>
