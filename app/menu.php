b25a407a2d0a6527337c896cdc84cc4e63fd73af
Hugo <bindwin@live.fr>
2014-11-28 09:43:47 +0100

    maj menu_co, menu.php, ucp_user

--- app/menu.php
+++ app/menu.php
@@ -1,12 +1,12 @@
 <?php 
 if (isset($_SESSION["id"])){
-	require("views/menu_co.phtml"); 
 	if($_SESSION['admin']){
 		$link="ucp_admin";
 	}
 	else{
 		$link="ucp_user";
 	}
+	require("views/menu_co.phtml");
 }
 else 
 	require("views/menu.phtml");
