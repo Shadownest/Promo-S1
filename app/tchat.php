25cbb6e3230af0c66638b3a651dcb1f949ea1110
Hugo <bindwin@live.fr>
2014-12-01 15:40:12 +0100

    ajout securité chatbox

--- app/tchat.php
+++ app/tchat.php
@@ -1,6 +1,7 @@
 <?php 
 if(isset($_POST["content"])) {
-		$res = mysqli_query($db,"INSERT INTO `forum`.`tchat` (`author`, `content`) VALUES ('".$_SESSION["id"]."', '".$_POST["content"]."')");
+		$content = mysqli_real_escape_string($_POST["content"]);
+		$res = mysqli_query($db,"INSERT INTO `forum`.`tchat` (`author`, `content`) VALUES ('".$_SESSION["id"]."', '".$content."')");
 	}
 	else if(isset($_GET["refresh"]) && $_GET["refresh"] == "true") {
 		require("app/bloc_tchat.php");
