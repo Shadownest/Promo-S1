91675fef64eefafbf88b0a2b7e63141d1129b230
Hugo <bindwin@live.fr>
2014-12-02 12:24:33 +0100

    design des sujet dans catégorie

---
+++ sources/js/all.js
@@ -0,0 +1,9 @@
+//hide show create subject
+$('document').ready(function()
+{
+	$(".create_subject").hide();
+
+	$(".show_create_subject").click(function(){
+		$(".create_subject").show();
+	});
+});
