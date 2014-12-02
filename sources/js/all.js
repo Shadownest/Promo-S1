b80797e4ae6ea877af2fcccb75c91dc971cc882e
Hugo <bindwin@live.fr>
2014-12-02 14:44:14 +0100

    phtml des sujet te message

--- sources/js/all.js
+++ sources/js/all.js
@@ -1,9 +1,21 @@
-//hide show create subject
 $('document').ready(function()
 {
+	//hide show create subject
 	$(".create_subject").hide();
 
 	$(".show_create_subject").click(function(){
 		$(".create_subject").show();
+	});
+
+	//hide show create message in subject
+
+	$(".new_message").hide();
+
+	$(".create_new_message").click(function(){
+		$(".new_message").show();
+	});
+
+	$("hide_bloc").click(function(){
+		$(".new_message").hide();
 	});
 });
