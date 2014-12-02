7e283978494a462d1a440252711c9c9cd223c9e4
mathieu67 <mathieu.unlu@gmail.com>
2014-12-02 11:34:11 +0100

    MAJ bloc subject

--- app/bloc_subject.php
+++ app/bloc_subject.php
@@ -1,37 +1,36 @@
 <?php 
 // la liste des sujects de chaque categorie
 
-if (!isset($_GET["id"]))
-{
+if($_GET['page']=="category"){
+	$id=$titleCategory->getId();
+}
+else{
 	$id=$homeCategory[$i]->getId();
-	$manager = new SubjectManager($db);
-	$homeSubject = $manager-> displayListSubject($id);
-
-
-	$j=0;
-	while($j<count($homeSubject))
-	{
-		$Subject=$homeSubject[$j];
-
-		$manager = new MessageManager($db);
-		$homeMessage = $manager->displayLastMessageOfSubject($Subject->GetId());
-
-		$Message= new Message($db);
-		$lastAuthor="Aucun message pour ce sujet";
-		if ($homeMessage)
-		{
-			$Message=$homeMessage[0];
-			
-			$lastAuthor="Dernier message par : ".$Message->getAuthor()->getName();
-		}
-		
-		require('views/bloc_subject.phtml');
-		$j++;
-	}
 }
-else
+
+$manager = new SubjectManager($db);
+$homeSubject = $manager-> displayListSubject($id);
+
+
+$j=0;
+while($j<count($homeSubject))
 {
-	$id=$_GET["id"];
-	require('app/bloc_message.php');
+	$Subject=$homeSubject[$j];
+
+	$manager = new MessageManager($db);
+	$homeMessage = $manager->displayLastMessageOfSubject($Subject->GetId());
+
+	$Message= new Message($db);
+	$lastAuthor="Aucun message pour ce sujet";
+	if ($homeMessage)
+	{
+		$Message=$homeMessage[0];
+		
+		$lastAuthor="Dernier message par : ".$Message->getAuthor()->getName();
+	}
+	
+	require('views/bloc_subject.phtml');
+	$j++;
 }
+
 ?>
